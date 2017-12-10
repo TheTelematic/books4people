0c90f89e54107a08761a116e76ea5ea9b25bf0ea
Arturo Parra <arturoteleco95@protonmail.ch>
2017-12-10 16:43:30 +0100

    BUG: ProgrammingError with the Book model

--- books4people/settings.py
+++ books4people/settings.py
@@ -36,9 +36,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'core',
     'books',
     'accounts',
-    'core'
 )
 
 MIDDLEWARE_CLASSES = (
